f55ffc795e5ea8a64624bbf31fc79e98ea089403
Lorien Aquarius <lorienaquarius@cmail.carleton.ca>
2023-04-18 23:20:22 -0400

    Added Titanium ingot, accessible using give <player> titaniummod:titanium_bar

--- src/main/java/net/sage7rules/titaniummod/TitaniumMod.java
+++ src/main/java/net/sage7rules/titaniummod/TitaniumMod.java
@@ -10,6 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.sage7rules.titaniummod.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -26,6 +27,8 @@ public class TitaniumMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -33,7 +36,7 @@ public class TitaniumMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
+
 
     }
 
---
+++ src/main/java/net/sage7rules/titaniummod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.sage7rules.titaniummod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sage7rules.titaniummod.TitaniumMod;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TitaniumMod.MODID);
+
+    public static final RegistryObject<Item> TITANIUM = ITEMS.register("titanium_bar",
+                                                        () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+
+        ITEMS.register(eventBus);
+
+    }
+
+}
